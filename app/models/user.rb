e9c0ea96c43f283cf477d668a102ab7766f471e4
KonradDabrowski99 <dabrowski.konrad@onet.pl>
2021-11-21 21:19:49 +0100

    User Password

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :trainings
   has_many :plans
   has_many :favourites
+
+  has_secure_password
+
+  validates :first_name, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :last_name, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :email, presence: true, uniqueness: true, length: { in: 3..50 }
+  validates :password, presence:true, length: { minimum: 6 }
 end
